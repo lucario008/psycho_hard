35a096d6767470a6962d74f1407ae92453237f8d
Lucario <lucagonzmar@gmail.com>
2025-01-04 12:01:20 +0100

    solo la linterna para la mezcla

--- project_psycho_hard/Assets/Scripts/IOperable.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-// Este archivo contiene solo la definición de la interfaz
-public interface IOperable
-{
-    void Operate(); // Declaración del método
-}
